3bfe41c05d051f0d292ac9cb3986f7dc88d14104
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-19 21:46:18 +0900

    Create List2-2.cs

---
+++ List2-2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            int number = 8;
+            Console.WriteLine("{0}歳の人は{1}人います", age, number);
+        }
+    }
+}
