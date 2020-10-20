c84fc87d4de2196a44e733333379ce2618870c84
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-20 21:45:08 +0900

    Create List2-5.cs

---
+++ List2-5.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 8;
+            int width = 5;
+            int area;
+            area = height * width;
+            Console.WriteLine("面積：{0}㎡", area);
+        }
+    }
+}
