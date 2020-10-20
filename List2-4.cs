1282545c984ebbabd3c57a94510f4d32f19a19d7
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-20 21:42:14 +0900

    Create List2-4.cs

---
+++ List2-4.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "山田";
+            Console.WriteLine("{0}さん、こんにちは", name);
+            name = "佐藤";
+            Console.WriteLine("{0}さん、お久しぶりです", name);
+        }
+    }
+}
