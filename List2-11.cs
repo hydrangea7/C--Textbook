c9c798253b15ad99834b9e579be4933427b29ea4
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-21 19:33:25 +0900

    Create List2-11.cs

---
+++ List2-11.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emptyString = "";
+            Console.WriteLine("1行目");
+            Console.WriteLine(emptyString);
+            Console.WriteLine("3行目");
+        }
+    }
+}
