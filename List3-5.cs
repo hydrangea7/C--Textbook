8426a603b95bc17c52371d083d59c9d1e3497837
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-21 20:28:34 +0900

    Create List3-5.cs

---
+++ List3-5.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 6;
+            var dup = num;
+            Console.WriteLine(num);
+            Console.WriteLine(dup);
+        }
+    }
+}
