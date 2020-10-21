3a822d72290f964c3226be2e644b51f14efdecbd
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-21 20:33:26 +0900

    Create List3-8.cs

---
+++ List3-8.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = 15.8;
+            var width = (int)source;
+            Console.WriteLine(width);
+        }
+    }
+}
