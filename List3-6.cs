67bddfed2f7fc2b3140e36ee2db3c06fb1354429
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-21 20:31:09 +0900

    Create List3-6.cs

---
+++ List3-6.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 8;
+            num += 2;
+            Console.WriteLine(num);
+            num *= 4;
+            Console.WriteLine(num);
+        }
+    }
+}
