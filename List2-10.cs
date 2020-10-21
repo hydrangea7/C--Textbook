17293c959f65626dab6cb18481f584748103f6b7
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-21 19:28:02 +0900

    Create List2-10.cs

---
+++ List2-10.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "おはようございます";
+            var length = message.Length;
+            Console.WriteLine("{0}文字", length);
+        }
+    }
+}
