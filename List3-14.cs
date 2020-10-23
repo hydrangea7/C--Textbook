b1b4bc9624a5afc20617becfa9c21a36c9c0320a
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-23 21:43:53 +0900

    Create List3-14.cs

---
+++ List3-14.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "こんにちは、";
+            message += "今日は良い天気ですね。";
+            Console.WriteLine(message);
+
+        }
+    }
+}
