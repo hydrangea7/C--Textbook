44311ca4a71e9d7c15260d194b1c3531001b00da
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-23 21:09:34 +0900

    Create List3-12.cs

---
+++ List3-12.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "これは正しい文字列です。" +
+                      "+演算子で連結させています。";
+            Console.WriteLine(str);
+        }
+    }
+}
