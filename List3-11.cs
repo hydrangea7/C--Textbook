80098a56d17f7ae9016a43e2f07a0918a109ac6a
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-23 21:07:05 +0900

    Create List3-11.cs

---
+++ List3-11.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "渡辺";
+            var message = name + "さん、おはようございます。";
+            Console.WriteLine(message);
+        }
+    }
+}
