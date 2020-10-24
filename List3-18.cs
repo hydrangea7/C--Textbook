31eb0143ec26450a93df29da9f4a5d2c8be02306
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-24 14:50:48 +0900

    Create List3-18.cs

---
+++ List3-18.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = '夏';
+            var temperature = 39;
+            Console.WriteLine($"今年の{season}の最高気温は、{temperature}度でした。");
+        }
+    }
+}
