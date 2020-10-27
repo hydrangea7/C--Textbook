c4679e0bfdba1af29fa2e01de43869de8a92c417
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-10-27 23:35:17 +0900

    Create List5-5.cs

---
+++ List5-5.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 5;
+            for (var i = 0; i < n; i++)
+            {
+                Console.WriteLine($"i = {i}");
+            }
+        }
+    }
+}
