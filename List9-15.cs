78b7a2dc991d759d33188c8fc0ad3026876e5924
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-11-01 23:36:56 +0900

    Create List9-15.cs

---
+++ List9-15.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+
+namespace ClassSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abs1 = Math.Abs(150);
+            var abs2 = Math.Abs(-320);
+            Console.WriteLine(abs1);
+            Console.WriteLine(abs2);
+        }
+    }
+}
