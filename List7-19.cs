12d54b16184fa7f2a74deab6622e61027b33820c
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-11-01 17:44:33 +0900

    Create List7-19.cs

---
+++ List7-19.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ClassSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var num = 10;
+            Console.WriteLine(num);
+            Print();
+            Console.WriteLine(num);
+        }
+
+        static void Print()
+        {
+            var num = 123;
+            Console.WriteLine(num);
+        }
+    }
+}
