b4ce71e26fbdbea5beb79711734dd32bfff1a936
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-11-01 21:21:10 +0900

    Create List9-10.cs

---
+++ List9-10.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+
+namespace ClassSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "HTML File";
+            var str2 = str.ToLower();
+            Console.WriteLine(str2);
+        }
+    }   
+}
