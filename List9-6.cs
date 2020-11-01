11ae27c5376154a837114a713fec6179114c8dbf
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-11-01 21:08:21 +0900

    Create List9-6.cs

---
+++ List9-6.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+
+namespace ClassSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "ようこそ、C#の世界へ";
+            var length = str.Length;
+            Console.WriteLine($"{length}文字です");
+        }
+    }   
+}
