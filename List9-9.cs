43730dbe0d700c2d8c5b531a8365f5957f6a9f20
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-11-01 21:19:40 +0900

    Create List9-9.cs

---
+++ List9-9.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+
+namespace ClassSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "Microsoft";
+            var str2 = str.ToUpper();
+            Console.WriteLine(str2);
+        }
+    }   
+}
