1e277940faab4776777c37d111b60bd7bf7eb8a6
hydrangea7 <71221161+hydrangea7@users.noreply.github.com>
2020-11-03 11:02:11 +0900

    Create List9-26.cs

---
+++ List9-26.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+
+namespace ClassSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new DateTime(2019, 4, 3, 19, 8, 53);
+            var s5 = date.ToString("yyyy年MM月dd日(ddd) HH時mm分ss秒");
+            var s6 = date.ToString("yy年M月d日(ddd) H時m分s秒");
+            var s7 = date.ToString("yy年M月d日(ddd) tth時m分s秒");
+            var s8 = date.ToString("yyyy/MM/dd HH:mm");
+            var s9 = date.ToString("yy/M/d H:m");
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+            Console.WriteLine(s8);
+            Console.WriteLine(s9);
+        }
+    }
+}
